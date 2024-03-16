afb67bba6292fd7ee2df1aac9fc2a8c7be17d495
amzxir <amirreza79ahmadi@gmail.com>
2024-03-17 02:47:41 +0330

    change icon

--- src/layouts/MianLayouts/sidebar.jsx
+++ src/layouts/MianLayouts/sidebar.jsx
@@ -16,27 +16,27 @@ const Sidebar = () => {
                     <li className="sidebar-header fw-bolder fs-lg">مدیریت آگهی ها</li>
                     <li className="sidebar-item">
                         <NavLink aria-current="page" className={({ isActive }) => isActive ? 'sidebar-link active' : 'sidebar-link'} to="/">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-video align-middle me-2">
-                                <polygon points="23 7 16 12 23 17 23 7"></polygon>
-                                <rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user align-middle me-2">
+                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
                             </svg>
                             <span className="align-middle me-2">همه آگهی ها</span>
                         </NavLink>
                     </li>
                     <li className="sidebar-item">
-                        <NavLink className={({isActive}) => isActive ? 'sidebar-link active' : 'sidebar-link'} to="create-adv">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-video align-middle me-2">
-                                <polygon points="23 7 16 12 23 17 23 7"></polygon>
-                                <rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect>
+                        <NavLink className={({ isActive }) => isActive ? 'sidebar-link active' : 'sidebar-link'} to="create-adv">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user align-middle me-2">
+                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
                             </svg>
                             <span className="align-middle me-2">ایجاد آگهی</span>
                         </NavLink>
                     </li>
                     <li className="sidebar-item">
-                        <NavLink className={({isActive}) => isActive ? 'sidebar-link active' : 'sidebar-link'} to="manage-adv">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-video align-middle me-2">
-                                <polygon points="23 7 16 12 23 17 23 7"></polygon>
-                                <rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect>
+                        <NavLink className={({ isActive }) => isActive ? 'sidebar-link active' : 'sidebar-link'} to="manage-adv">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user align-middle me-2">
+                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
                             </svg>
                             <span className="align-middle me-2">مدیریت اگهی ها</span>
                         </NavLink>
